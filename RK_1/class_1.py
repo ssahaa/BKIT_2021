7a09aefca6fa4064d03c31db36f656dc2528578d
ssahaa <63342063+ssahaa@users.noreply.github.com>
2021-10-23 15:05:33 +0300

    Create class_1.py

---
+++ RK_1/class_1.py
@@ -0,0 +1,13 @@
+"""
+класс_1
+"""
+class Book:
+
+    def __init__(self,book_id,name,lists,num_shop):
+        self.name_book = name
+        self.lists = lists
+        self.num_shop = num_shop
+        self.book_id = book_id
+
+    def __repr__(self):
+        return ""
