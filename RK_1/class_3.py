0cec1427af9938a809545a4f9d4394b9219335fe
ssahaa <63342063+ssahaa@users.noreply.github.com>
2021-10-23 15:06:11 +0300

    Create class_3.py

---
+++ RK_1/class_3.py
@@ -0,0 +1,9 @@
+"""
+Связующее звено
+"""
+class book_and_shop:
+
+    def __init__(self,num_shop_ID,book_id_of_shop):
+        self.book_id_Sh_ = book_id_of_shop
+        self.num_shop_ID = num_shop_ID
+
